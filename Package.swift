6b912012ce07b036e7d2fff98bba365c0a8c4753
karkadi <karkadi@gmail.com>
2022-05-16 21:30:18 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,10 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftKeychainWrapper",
+    platforms: [.macOS(.v10_12),
+                .iOS(.v10),
+                .tvOS(.v10),
+                .watchOS(.v3)],
     products: [
         .library(name: "SwiftKeychainWrapper", targets: ["SwiftKeychainWrapper"])
     ],
